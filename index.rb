56d6e6697e5ade251024f026ccec4003fb1556b7
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-05-11 14:21:57 -0300

    comitt doidauw

--- index.rb
+++ index.rb
@@ -2,4 +2,5 @@ first_arr = ['a', 'b', 'c', 'd', 'e']
 second_arr = ['c', 'd', 'e', 'f', 'g']
 
 
-puts ((first_arr - second_arr) | (second_arr - first_arr)).uniq
+puts ((first_arr - second_arr, qualqeurmeda) | (second_arr - first_arr)).uniq
+# giovanny testing muito loco
